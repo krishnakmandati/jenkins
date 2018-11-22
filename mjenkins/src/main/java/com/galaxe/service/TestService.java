a62126b7169fc5d6b209a19ca4bae744622bef85
kmandati <kmandati@gbwk-INA02209LY.galaxy.lan>
2018-11-22 12:55:24 +0530

    Initial version of test code

---
+++ mjenkins/src/main/java/com/galaxe/service/TestService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.galaxe.service;
+
+/**
+ * @author kmandati
+ *
+ */
+public class TestService {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Jenkins Welcome!!!");
+	}
+
+}
